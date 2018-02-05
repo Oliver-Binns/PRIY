3895407fd75208c7fe32692f0db28377829febf4
Oliver <mail@oliverbinns.co.uk>
2018-02-05 16:36:00 +0000

    Refactored LTi and LTin move functionality to share code.

--- FlameGPU/src/model/functions.c
+++ FlameGPU/src/model/functions.c
@@ -75,43 +75,27 @@ __FLAME_GPU_FUNC__ glm::vec2 boundPosition(glm::vec2 agent_position){
  * @return Return type is always int. 0 means the agent does NOT die.
  */
 __FLAME_GPU_FUNC__ int lti_random_move(xmachine_memory_LTi* xmemory,
-									   xmachine_message_location_list* location_messages,
-                                       RNG_rand48* rand48)
+    xmachine_message_location_list* location_messages,
+    RNG_rand48* rand48)
 {
-    glm::vec2 agent_position = glm::vec2(xmemory->x, xmemory->y);
+    glm::vec2 init_position = glm::vec2(xmemory->x, xmemory->y);
+    glm::vec2 new_position = random_move(init_position, xmemory->velocity, rand48);
     
-    //Calculate velocity
-    float angle = 2 * M_PI *  rnd<CONTINUOUS>(rand48);
-    float x_move = xmemory->x * sinf(angle);
-    float y_move = xmemory->y * cosf(angle);
-
-    glm::vec2 agent_velocity = glm::vec2(x_move, y_move);
-
-    agent_position += agent_velocity;
-    agent_position = boundPosition(agent_position);
-
-    xmemory->x = agent_position.x;
-    xmemory->y = agent_position.y;
-  
+    xmemory->x = new_position.x;
+    xmemory->y = new_position.y;
+    
     return 0;
 }
 
 __FLAME_GPU_FUNC__ int ltin_random_move(xmachine_memory_LTin* xmemory,
-                                        xmachine_message_location_list* location_messages,
-                                        RNG_rand48* rand48)
+    xmachine_message_location_list* location_messages,
+    RNG_rand48* rand48)
 {
-    glm::vec2 agent_position = glm::vec2(xmemory->x, xmemory->y);
+    glm::vec2 init_position = glm::vec2(xmemory->x, xmemory->y);
+    glm::vec2 new_position = random_move(init_position, xmemory->velocity, rand48);
     
-    //Calculate velocity
-    float angle = 2 * M_PI *  rnd<CONTINUOUS>(rand48);
-    float x_move = xmemory->x * sinf(angle);
-    float y_move = xmemory->y * cosf(angle);
-
-    glm::vec2 agent_velocity = glm::vec2(x_move, y_move);
-	agent_position = boundPosition(agent_position);
-    
-    xmemory->x = agent_position.x;
-    xmemory->y = agent_position.y;
+    xmemory->x = new_position.x;
+    xmemory->y = new_position.y;
     
     return 0;
 }
